7cd6e5fa20252b9b7dc7af48c8d79aeef052f029
unknown <pranavmenon2468@gmail.com>
2016-01-25 19:31:27 -0500

    Deleted files

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-
-public class Main {
-	public static void main(String args[]){
-		ArrayList<Sentence> allSentencesList = ArticleTextFetcher.getFormattedArticleText("abc");
-		SummaryInformation summaryInfo = Summarizer.createSummary(allSentencesList);
-	}
-}
--- src/Sentence.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Sentence {
-	ArrayList<String> words;
-	
-	public Sentence(ArrayList<String> words){
-		this.words = words;
-	}
-}
